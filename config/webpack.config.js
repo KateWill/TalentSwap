f92526a245032f730952cc8b437d651bbf62c668
Katelyn  Williamson <katew@Katelyns-MacBook-Pro.local>
2017-03-09 20:40:56 -0700

    Started-app

---
+++ config/webpack.config.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: {
+    vendor: path.join(__dirname, '../client/vendor.js'),
+    application: path.join(__dirname, '../client/index.js')
+  },
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js",
+    path: path.resolve(path.join(__dirname, '..', 'public'))
+  },
+  resolve: {
+    modules: ['client', 'node_modules'],
+    extensions: [".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        include: [ path.join(__dirname, "..", "client")],
+        options: { cacheDirectory: true }
+      },
+      { 
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader:"url-loader", 
+        query: { 
+          limit: 10000, 
+          mimetype: "application/font-woff" 
+        }
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader", query: { sourceMap: true }},
+          {
+            loader: "css-loader",
+            query: {
+              modules: true,
+              importLoaders: true,
+              localIdentName: '[local]___[hash:base64:5]'
+            }
+          },
+          { loader: "sass-loader" },
+          { loader: "postcss-loader" }
+        ],
+        exclude: /node_modules/,
+        include: [ path.join(__dirname, "..", "client")],
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ options: { postcss: [ autoprefixer ] }}),
+    new ExtractTextPlugin('styles.css'),
+  ],
+  devtool: 'source-map'
+};
