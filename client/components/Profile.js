734c8eac0cb168e0102ad15fd4bbe0ac702683aa
Katelyn  Williamson <katew@Katelyns-MacBook-Pro.local>
2017-03-15 19:58:18 -0600

    profile

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Profile = () => (
   <div className="container">
   <form>
   <label>
     <h2>Enter your profile information here</h2>
-    <input type="text" name="name" />
-      <input type="text" name="location" />
-        <input type="text" name="skills" />
+    <input type="text" required={true} ref={ n => this.name = n}  placeholder="name" />
+      <input type="text" required={true} ref={ n => this.name = n}  placeholder="location" />
+        <input type="text" required={true} ref={ n => this.name = n} placeholder="skills" />
   </label>
-  <input type="submit" value="Submit" />
+  <button className="btn yellow darken-2" type="submit" value="Submit">Create</button>
 </form>
   </div>
 )
 
-export default Profile;
+export default connect() (Profile);
