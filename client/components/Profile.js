e5ff1ddfc6c5f84b5cb66295e5ba5f056ff64a46
Katelyn  Williamson <katew@Katelyns-MacBook-Pro.local>
2017-03-25 12:01:58 -0600

    profile cards updated

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -3,25 +3,30 @@ import React from 'react';
 const Profile = ({username,zipcode,talent}) => (
   <div>
     <div className="col s4">
-      <div className="card indigo lighten-4">
-        <div className="card-content">
-          <span className="card-title">Talent: {talent}</span>
-          <p>
+      
+    <div className="card">
+      <div className="card-image waves-effect waves-block waves-light">
+        <img className="activator" src="images/mainimg1.jpg" />
+      </div>
+      <div className="card-content">
+        <span className="card-title activator grey-text text-darken-4">Talent: {talent}<i className="material-icons right">more_vert</i></span>
+      </div>
+      <div className="card-reveal">
+        <span className="card-title grey-text text-darken-4">Talent: {talent}<i className="material-icons right">close</i></span>
+        <p>
             Username: {username}<br/>
             Zipcode: {zipcode}<br/>
-          </p>
+        </p>
+        <div className="card-action">
+            <a href="#"><i className="tiny material-icons">comment</i> Comment</a>
+            <a href="#"><i className="tiny material-icons">thumb_up</i> Like</a>
         </div>
-        <center>
-        <div className="card-action indigo lighten-2">
-          <a href="#"><i className="tiny material-icons">stars</i> Save</a>
-          <a href="#"><i className="tiny material-icons">thumb_up</i> Like</a>
-          <a href="#"><i className="tiny material-icons">thumb_down</i> Hide</a>
+
         </div>
-        </center>
-      </div>
-      <br/><br/>
       </div>
     </div>
+   
+  </div>
   
 )
 
--- client/components/Profiles.js
+++ client/components/Profiles.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 //import { refreshLogin } from '../actions/profiles';
 import { setFlash } from '../actions/flash';
 import Profile from './Profile'
-import {searchUser} from '../actions/search';
-
 
 class Profiles extends React.Component {
   state = { users: [], talent: '', username: '', zipcode:'' }
@@ -57,43 +55,19 @@ class Profiles extends React.Component {
       <div>
         <div className="container">
           
-          <center><h1>Search Profiles </h1>
-          <p><i> Or If you're feeling adventurous click "Explore" for a randomized profile</i></p></center>
+          <center><h1>Search Profiles </h1></center>
           <div className="row">
             <center>
               <form onSubmit={this.handleSubmit}>
                 <i className="small material-icons">search</i>
                 <input style={{width:"350px", fontSize:"20px", height: "35px"}} type="Talent" ref={ n => this.talent = n } placeholder=" Talent " /><br/><br/>
                 <button type="submit" style={{fontSize:"20px", height: "50px", marginRight: "20px"}} className="btn yellow darken-2"name="uprofile">Search</button>
-                <button type="button" style={{fontSize:"20px", height: "50px"}} className="btn yellow darken-2"name="uprofile">Explore</button>
               </form>
               
             </center>
 
               <br/>
-              <div>
-                <h3>Featured Search Result...</h3>
-                <p>This is will be the top result? And the other matches will be underneath?</p>
-                <div className="card indigo lighten-4">
-                  <div className="card-content">
-                    <span className="card-title">Talent: {this.state.talent}</span>
-                    <p>
-                      Username: {this.state.username} <br/>
-                      Zipcode: {this.state.zipcode}<br/><br/>
-
-                      <button className="btn yellow darken-2"name="uprofile">Contact</button>
-
-                    </p>
-                  </div>
               
-                <center> <div className="card-action indigo lighten-2">
-                <a href="#"><i className="tiny material-icons">stars</i> Save</a>
-                <a href="#"><i className="tiny material-icons">thumb_up</i> Like</a>
-                <a href="#"><i className="tiny material-icons">thumb_down</i> Hide</a>
-                </div></center>
-            </div>
-          </div>
-            
             <div>
               <h4>All Profiles: </h4>
                 {users}     
