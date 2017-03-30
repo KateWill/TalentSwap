6d88f66035487f05d521899e542225571c4d3b30
Zachary 2 <zteagar@msn.com>
2017-03-29 20:23:09 -0600

    stylying

--- client/components/Home.js
+++ client/components/Home.js
@@ -7,9 +7,9 @@ class Home extends React.Component {
     var inlineStyle = {
       h1: {color:"white", left:"38%",top:"40%",fontWeight:"300", position:'absolute',textShadow: "1px 1px #404040"},
       img: {position: 'relative',width:"100%"},
-      h5: {color:"white",fontWeight:"200", left:"42%",top:"50%", position:'absolute',textShadow: "1px 1px #404040",padding:"20px"},
+      h5: {color:"white",fontWeight:"200", left:"41%",top:"50%", position:'absolute',textShadow: "1px 1px #404040",padding:"20px"},
       p: {color:"black",margin:"20px",textAlign:"center",padding:"10px",fontFamily:"san-serif",fontSize:"20px"},
-      button: {position:"relative", left:"2%",margin:"5%"}
+      btn: {position:"relative", left:"2%"}
   }
    var style2 = {
      h5: {color:"black"}
@@ -22,7 +22,7 @@ class Home extends React.Component {
           <h1 style={inlineStyle.h1}>Talent Swap</h1>
           
            <h5 style={inlineStyle.h5}>Connect with others <br/> and get to <i style={{color:"#4db6ac"}}>swapping</i>
-           <br /><button style={inlineStyle.button} className="btn #4db6ac darken-2 pulse">Register</button> </h5>  
+           <br /><br/>&nbsp;&nbsp;&nbsp;<a href="/register" className="btn">Register </a>   </h5>
            
       <div className="row" className="col s12">
         
